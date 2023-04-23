a1d97046ec7f8b56f9d9962c401687eb7c265d81
Yann Letouzey <y.letouzey@gmail.com>
2023-04-23 11:51:53 +0200

    change class name footer + add js curveText

--- js/app.js
+++ js/app.js
@@ -8,3 +8,22 @@ spanText.forEach((element) => {
     }s">${e}</span>`;
   });
 });
+
+
+const title = document.getElementById('title')
+function curveText(id) {
+    const textWithId = document.getElementById('' + id)
+    const text = document.getElementById('' + id).textContent
+    textWithId.innerHTML = ""
+    const deg = 150
+    const textArray = Array.from(text)
+    const partDeg = deg / (textArray.length - 1)
+    let d = -deg / 2
+    for (let i = 0; i < textArray.length; i++) {
+        d = Math.trunc(d)
+        textWithId.innerHTML += `<span id="${textArray[i]}" style="transform: rotate(${d}deg);">${textArray[i]}</span>`
+        d += partDeg
+    }
+}
+//Enter id tag text only (p/h1/h2/...)
+curveText("title")
