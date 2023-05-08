eca6b95f54c94605bfc2da5deb5c16a63682a6f4
Yann Letouzey <y.letouzey@gmail.com>
2023-05-08 13:43:41 +0200

    fix error js btn more-info

--- js/morePage.js
+++ js/morePage.js
@@ -11,30 +11,10 @@ const morePageFour = document.querySelector('.website-four')
 let widthFace = document.getElementById('main__container--carousel-face').clientWidth / 16
 
 function showPageMore(btn, page){
-    btn.addEventListener('click', () => {
-        page.style.display = "block"
-        // page.style.width = "100%"
-        // page.style.maxWidth = "60rem"
-
-        // if (window.innerWidth >= (350 + (widthFace * 16))) {
-        //     page.style.maxWidth = (window.innerWidth - 300) + "px"
-        //     page.style.width = (widthFace * 5) + "rem"
-        //     window.addEventListener('resize', () => {
-        //         page.style.minWidth = ((widthFace * 16) + (widthFace / 2)) + "px"
-        //         page.style.maxWidth = (window.innerWidth - 300) + "px"
-        //         page.style.width = (widthFace * 5) + "rem"
-        //     })
-        // } else {
-        //     page.style.width = widthFace + "rem"
-        //     window.addEventListener('resize', () => {
-        //         if (window.innerWidth >= (350 + (widthFace * 16))) {
-        //             page.style.minWidth = ((widthFace * 16) + (widthFace / 2)) + "px"
-        //             page.style.maxWidth = (window.innerWidth - 300) + "px"
-        //             page.style.width = (widthFace * 5) + "rem"
-        //         }
-        //     })
-        // }
-    })
+    // btn.addEventListener('click', () => {
+    //     page.style.display = "block"
+        
+    // })
 }
 showPageMore(btnMoreOne, morePageOne)
 showPageMore(btnMoreTwo, morePageTwo)
